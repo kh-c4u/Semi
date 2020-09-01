a5e41a106f10c9d944bb1c2919489a4722f5d0c7
han-jungwon <rhehf1024@naver.com>
2020-09-01 19:42:00 +0900

    ㄴㄹㅇㄴㅇㄹ

--- src/com/kh/semi/buymarket/controller/BuyMarketSelectOneServlet.java
+++ src/com/kh/semi/buymarket/controller/BuyMarketSelectOneServlet.java
@@ -20,7 +20,7 @@ import com.kh.semi.buymarketComment.model.vo.BuyMarketComment;
 @WebServlet("/buymarketselectOne.bo")
 public class BuyMarketSelectOneServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	ㅇㄹㄴㅇㄹ
+	ㅇㄹㄴㅇㄹㄴㅇ
        
     /**
      * @see HttpServlet#HttpServlet()
